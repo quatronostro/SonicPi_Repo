abc51e81ce9ed7e02666f304526fbb072d935ccf
Berke <93223660+quatronostro@users.noreply.github.com>
2023-03-31 16:37:11 +0300

    Create furElise.rb

---
+++ MimarS/furElise.rb
@@ -0,0 +1,20 @@
+# Für Elise - Sonic Pi version
+use_bpm 60
+use_synth :piano
+
+# Define the notes
+notes = [:e5, :ds5, :e5, :ds5, :e5, :b4, :d5, :c5,
+         :a4, :rest, :c4, :e4, :a4, :b4, :rest, :e4,
+         :gs4, :b4, :c5, :rest, :e4, :rest, :a4, :rest,
+         :b4, :rest, :e4, :rest, :e5, :ds5, :e5, :ds5,
+         :e5, :b4, :d5, :c5, :a4, :rest, :c4, :e4, :a4,
+         :b4, :rest, :e4, :gs4, :b4, :c5, :rest, :e4, :rest,
+         :a4, :rest, :b4, :rest, :e4]
+
+# Play the notes and sleep for 0.25s between each note
+live_loop :fur_elise do
+  notes.each do |n|
+    play n
+    sleep 0.25
+  end
+end
